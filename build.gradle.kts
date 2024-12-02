5a04f5f2479e748070332fbcc14c37fdaa111301
Kyrylo Semenchenko <kyrylo.semenhcenko@gmail.com>
2024-12-02 09:45:58 +0100

    day1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,25 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    application
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+application {
+    mainClass.set("Day01Kt") // Ensure this matches the file name without the .kt extension
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "Day01Kt" // Ensure this matches the file name without the .kt extension
+    }
+}
 sourceSets {
     main {
         kotlin.srcDir("src")
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,23 @@
+import java.io.File
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val firstNum = mutableListOf<Int>()
+    val secondNum = mutableListOf<Int>()
+
+    File("src/Day01.txt").forEachLine { line ->
+        val numbers = line.split("   ").map { it.toInt() }
+        firstNum.add(numbers[0])
+        secondNum.add(numbers[1])
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    firstNum.sort()
+    secondNum.sort()
+
+    var result = 0
+    for (i in firstNum.indices) {
+        result += abs(firstNum[i] - secondNum[i])
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(result)
 }
